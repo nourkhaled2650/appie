232ad6316eb85b7b202922c0064361be29986d2e
Nour Eldin <nourkhaled2650@gmail.com>
2023-02-22 06:08:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <Navbar></Navbar>
+    <FirstSection></FirstSection>
+    <SecondSection></SecondSection>
+    <ThirdSection></ThirdSection>
+    <FourthSection></FourthSection>
+    <FithSection></FithSection>
+    <SixthSection></SixthSection>
+    <Footer></Footer>
+  </div>
+);
+
+export default App;
